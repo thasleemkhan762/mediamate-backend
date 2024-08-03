5afe21aa6fa116cab096ecf8311550eb7e53958f
thasleemkhan762 <155559104+thasleemkhan762@users.noreply.github.com>
2024-08-03 14:34:39 +0530

    added userid for posts

--- models/postModel.js
+++ models/postModel.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const postSchema = mongoose.Schema(
   {
-    // userId: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "User",
-    //   required: true,
-    // },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     image: {
       type: String,
       required: [true, "please add the image"],
