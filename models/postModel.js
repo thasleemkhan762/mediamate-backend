8641474da852ff72d6e0365ac29551353b1b36de
thasleemkhan762 <155559104+thasleemkhan762@users.noreply.github.com>
2024-08-07 15:25:42 +0530

    post model updated with user id

--- models/postModel.js
+++ models/postModel.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 
 const postSchema = mongoose.Schema(
   {
-    // userId: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "User",
-    //   required: true,
-    // },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
     email: {
       type: String,
       required: [true, "Please add the user email"],
