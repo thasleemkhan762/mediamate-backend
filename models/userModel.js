5760c2a57e10bb803fccf802fc26be406dd02248
thasleemkhan762 <155559104+thasleemkhan762@users.noreply.github.com>
2024-08-13 10:08:02 +0530

    usewr model updated with gender, dob and place

--- models/userModel.js
+++ models/userModel.js
@@ -1,36 +1,50 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: [true, "Please add the username"],
+      type: String,
+      required: [true, "Please add the username"],
     },
     email: {
-        type: String,
-        required: [true, "Please add the user email"],
-        unique: [true, "This email is already in use"]
+      type: String,
+      required: [true, "Please add the user email"],
+      unique: [true, "This email is already in use"],
     },
     password: {
-        type: String,
-        required: [true, "Please add the user password"],
+      type: String,
+      required: [true, "Please add the user password"],
     },
     role: {
-        type: String,
-        enum: ["user", "admin"],
-        default: "user",
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      default: null,
+    },
+    dateOfBirth: {
+      type: Date,
+      default: null,
+    },
+    place: {
+      type: String,
+      default: null,
     },
     otp: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     otpExpiration: {
-        type: Date,
-        required: false
-    }
- 
-}, {
+      type: Date,
+      required: false,
+    },
+  },
+  {
     timestamps: true,
-}
+  }
 );
 
 module.exports = mongoose.model("User", userSchema);
