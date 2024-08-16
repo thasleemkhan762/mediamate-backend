13681f492d2dcb319547c3ee709a7c60f2a3458e
thasleemkhan762 <155559104+thasleemkhan762@users.noreply.github.com>
2024-08-16 14:53:28 +0530

    nw message or message saving controller function added

--- controllers/chatControllers.js
+++ controllers/chatControllers.js
@@ -3,13 +3,13 @@ const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const Chat = require("../models/chatModel");
 
-
+/// get all chats of the user
 const getUserChats = asyncHandler(async (req, res) => {
 
     try {
 
         const chats = await Chat.find({ participants: req.params.userId }).populate('participants messages.sender');
-        
+
         res.json(chats);
 
     } catch (err) {
@@ -20,7 +20,40 @@ const getUserChats = asyncHandler(async (req, res) => {
 
 });
 
-const sendMessage = asyncHandler(async (req, res) => {});
+// send new messages ( update chat or create new )
+const sendMessage = asyncHandler(async (req, res) => {
+
+    const { senderId, recipientId, content } = req.body;
+
+    try {
+
+        let chat = await Chat.findOne({ participants: { $all: [senderId, recipientId] } });
+
+        if (!chat) {
+
+            chat = new Chat({
+                
+              participants: [senderId, recipientId],
+              messages: [{ sender: senderId, content }],
+
+            });
+
+        } else {
+
+            chat.messages.push({ sender: senderId, content });
+        }
+
+        await chat.save();
+        
+        res.json(chat);
+
+    } catch (err) {
+
+        res.status(500).json({ error: 'Server error' });
+
+    }
+
+});
 
 module.exports = { getUserChats, sendMessage };
   
