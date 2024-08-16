e655dea9d3cdf045056b00c7617eac332376e77d
thasleemkhan762 <155559104+thasleemkhan762@users.noreply.github.com>
2024-08-16 14:47:51 +0530

    get all chat sfor a user controller function

--- controllers/chatControllers.js
+++ controllers/chatControllers.js
@@ -1,8 +1,24 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const Chat = require("../models/chatModel");
 
-const getUserChats = asyncHandler(async (req, res) => {});
+
+const getUserChats = asyncHandler(async (req, res) => {
+
+    try {
+
+        const chats = await Chat.find({ participants: req.params.userId }).populate('participants messages.sender');
+        
+        res.json(chats);
+
+    } catch (err) {
+
+        res.status(500).json({ error: 'Server error' });
+
+    }
+
+});
 
 const sendMessage = asyncHandler(async (req, res) => {});
 
